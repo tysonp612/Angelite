d7e4faa024ae98b9692e4372104757e19344e4f6
email <phamson061297@gmail.com>
2023-12-26 21:30:02 -0400

    finish connecting mongodb and project

---
+++ Model/database/express.js
@@ -0,0 +1,29 @@
+// express.js
+
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config({ path: "Model/database/.env" });
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.error("Error connecting to database:", err);
+  });
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
