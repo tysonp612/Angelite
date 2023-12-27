ef9860d06bc720c4e3cc9fce8f85da134890f371
email <phamson061297@gmail.com>
2023-12-26 21:59:08 -0400

    adding schema for bookings

---
+++ Model/database/schemas/bookings.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const bookingsSchema = {
+  client: {
+    type: ObjectId,
+    ref: "Client",
+  },
+  date: {
+    type: Date,
+  },
+  services: [
+    {
+      _id: {
+        type: ObjectId,
+        ref: "Services",
+      },
+    },
+  ],
+  price: {
+    type: Number,
+  },
+  note: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ["Processing", "Finished"],
+    default: "Processing",
+  },
+  timeOfBooking: {
+    type: Number,
+  },
+  period: {
+    type: Number,
+  },
+};
+module.exports = mongoose.model("Bookings", bookingsSchema);
