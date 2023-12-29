b203ca9e3892d882ddb98dcd44ee8495cd71573f
email <phamson061297@gmail.com>
2023-12-29 00:02:10 -0400

    finish adding router for client

--- Model/database/routes/client.js
+++ Model/database/routes/client.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const servicesController = require("./../controllers/services");
 
-router.route("/createService").post(servicesController.createService);
-router.route("/getAllServices").get(servicesController.getAllServices);
-router.route("/deleteService").post(servicesController.deleteService);
-router.route("/editService").post(servicesController.editService);
+const clientController = require("../controllers/client");
 
-
+router.route("/createClient").post(clientController.createClient);
+router.route("/findClient").post(clientController.findClient);
+router.route("/editClient").post(clientController.editClient);
+router.route("/deleteClient").post(clientController.deleteClient);
