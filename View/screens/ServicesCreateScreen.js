19dad3a2881c01e0be9bce3ab338f91b210e11f5
email <phamson061297@gmail.com>
2024-01-06 01:16:59 -0400

    finish adding duration picker for ServiceCreate screen

--- View/screens/ServicesCreateScreen.js
+++ View/screens/ServicesCreateScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
@@ -16,60 +16,76 @@ const ServiceCreateScreen = () => {
   const [description, setDescription] = useState("");
   const [color, setColor] = useState("red");
   const [duration, setDuration] = useState({ hours: "1", minutes: "30" });
-  const [isColorPickerVisible, setIsColorPickerVisible] = useState(false);
-  const [isDurationPickerVisible, setIsDurationPickerVisible] = useState(false);
+  const [isPickerVisible, setIsPickerVisible] = useState(false);
 
-  const toggleColorPicker = () =>
-    setIsColorPickerVisible(!isColorPickerVisible);
-  const toggleDurationPicker = () =>
-    setIsDurationPickerVisible(!isDurationPickerVisible);
+  const openPicker = () => {
+    if (isPickerVisible === false) {
+      setIsPickerVisible(true);
+    }
+  };
+  const closePicker = () => {
+    if (isPickerVisible === true) {
+      setIsPickerVisible(false);
+    }
+  };
+  // const toggleDurationPicker = () =>
+  //   setIsDurationPickerVisible(!isDurationPickerVisible);
 
   const handleColorChange = (selectedColor) => {
     setColor(selectedColor);
-    toggleColorPicker();
+    togglePicker();
   };
 
   const handleDurationChange = (selectedHours, selectedMinutes) => {
     setDuration({ hours: selectedHours, minutes: selectedMinutes });
-    toggleDurationPicker();
   };
 
-  const renderColorPicker = () => (
-    <Picker
-      selectedValue={color}
-      onValueChange={(itemValue) => handleColorChange(itemValue)}
-    >
-      <Picker.Item label="Red" value="red" />
-      <Picker.Item label="Blue" value="blue" />
-      <Picker.Item label="Green" value="green" />
-    </Picker>
-  );
+  // const renderColorPicker = () => (
+  //   <Picker
+  //     selectedValue={color}
+  //     onValueChange={(itemValue) => handleColorChange(itemValue)}
+  //   >
+  //     <Picker.Item label="Red" value="red" />
+  //     <Picker.Item label="Blue" value="blue" />
+  //     <Picker.Item label="Green" value="green" />
+  //   </Picker>
+  // );
 
   const renderDurationPicker = () => (
-    <Modal>
-      <Picker
-        selectedValue={duration.hours}
-        onValueChange={(itemValue) =>
-          handleDurationChange(itemValue, duration.minutes)
-        }
-      >
-        <Picker.Item label="1 hour" value="1" />
-        <Picker.Item label="2 hours" value="2" />
-        {/* Add more duration options as needed */}
-      </Picker>
+    <View style={styles.pickerWrapper}>
+      <View style={styles.pickerContainer}>
+        <Text style={styles.pickerText}>Hour:</Text>
+        <Picker
+          style={styles.picker}
+          selectedValue={duration.hours}
+          onValueChange={(itemValue) =>
+            handleDurationChange(itemValue, duration.minutes)
+          }
+        >
+          <Picker.Item label="1 hour" value="1" />
+          <Picker.Item label="2 hours" value="2" />
+          <Picker.Item label="3 hours" value="3" />
+          {/* Add more duration options as needed */}
+        </Picker>
+      </View>
 
-      <Picker
-        selectedValue={duration.minutes}
-        onValueChange={(itemValue) =>
-          handleDurationChange(duration.hours, itemValue)
-        }
-      >
-        <Picker.Item label="15 minutes" value="15" />
-        <Picker.Item label="30 minutes" value="30" />
-        <Picker.Item label="45 minutes" value="45" />
-        {/* Add more duration options as needed */}
-      </Picker>
-    </Modal>
+      <View style={styles.pickerContainer}>
+        <Text style={styles.pickerText}>minutes:</Text>
+        <Picker
+          style={styles.picker}
+          selectedValue={duration.minutes}
+          onValueChange={(itemValue) =>
+            handleDurationChange(duration.hours, itemValue)
+          }
+        >
+          <Picker.Item label="0 minutes" value="0" />
+          <Picker.Item label="15 minutes" value="15" />
+          <Picker.Item label="30 minutes" value="30" />
+          <Picker.Item label="45 minutes" value="45" />
+          {/* Add more duration options as needed */}
+        </Picker>
+      </View>
+    </View>
   );
 
   return (
@@ -97,22 +113,33 @@ const ServiceCreateScreen = () => {
       />
 
       <Text style={styles.label}>Color:</Text>
-      <TouchableOpacity
-        style={styles.input}
-        onPress={() => toggleColorPicker()}
-      >
-        <Text>{color}</Text>
-      </TouchableOpacity>
-      {renderColorPicker()}
+
+      {/* colour component */}
+      {/* <View style={styles.modalContainer}>
+        <TouchableOpacity style={styles.input} onPress={() => togglePicker()}>
+          <Text>{color}</Text>
+        </TouchableOpacity>
+        {isPickerVisible ? renderColorPicker() : ""}
+      </View> */}
 
       <Text style={styles.label}>Duration:</Text>
       <TouchableOpacity
-        style={styles.input}
-        onPress={() => toggleDurationPicker()}
+        style={styles.durationInput}
+        onPress={() => openPicker()}
       >
-        <Text>{`${duration.hours} hours ${duration.minutes} minutes`}</Text>
+        <View style={styles.durationInfoContainer}>
+          <Text
+            style={styles.durationInfo}
+          >{`${duration.hours} hours ${duration.minutes} minutes`}</Text>
+          {isPickerVisible ? (
+            <Button title="X" onPress={() => closePicker()} />
+          ) : (
+            ""
+          )}
+        </View>
+
+        {isPickerVisible ? renderDurationPicker() : ""}
       </TouchableOpacity>
-      {/* {renderDurationPicker()} */}
     </View>
   );
 };
@@ -129,17 +156,49 @@ const styles = StyleSheet.create({
     height: 40,
     borderColor: "gray",
     borderWidth: 1,
+    borderRadius: 15,
     marginBottom: 16,
     padding: 8,
   },
+  durationInput: {
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 15,
+    padding: 8,
+    minHeight: 40,
+  },
+  durationInfo: {
+    padding: 10,
+    fontSize: 17,
+  },
   modalContainer: {
-    // transparent: "true",
-    // animationType: "slide",
-    // visible: `${isColorPickerVisible}`,
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 15,
+  },
+  pickerContainer: {
+    flex: 1,
+    width: "100%",
+    flexDirection: "column",
+  },
+  picker: {
+    width: "100%",
+  },
+  pickerWrapper: {
+    flexDirection: "row",
+    paddingTop: 10,
+  },
+  pickerText: {
+    fontSize: 17,
+    textAlign: "center",
+  },
+  durationInfoContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  pickerCloseButton: {
+    textAlign: "right",
   },
 });
 
